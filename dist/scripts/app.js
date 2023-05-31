5fe07374a1f2439c01accdba35e3469102759342
Sina Kamrava <sinakamrava3118@gmail.com>
2023-05-31 19:54:14 +0330

    feat:add pwa option

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -13,6 +13,15 @@ const storage = new Catch();
   }
 })();
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
+
 
 const searchInput = document.getElementById('search-input');
 const searchModal = document.getElementById('search-modal');
